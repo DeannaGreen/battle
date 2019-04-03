5b281549f2439c154958e19e59eabdddffe4befd
Deanna Green <deanna@16vmini.co.uk>
2019-04-03 12:00:25 +0100

    switches players after turn

--- lib/game.rb
+++ lib/game.rb
@@ -1,6 +1,6 @@
 class Game
   attr_reader :current_turn
-  
+
   def initialize(player_1, player_2)
     @players = [player_1, player_2]
     @current_turn = player_1
@@ -16,5 +16,13 @@ class Game
 
   def attack(player)
     player.receive_damage
+  end
+
+  def switch_turns
+    @current_turn = opponent_of(current_turn)
+  end
+
+  def opponent_of(the_player)
+    @players.select { |player| player != the_player }.first
   end
 end
